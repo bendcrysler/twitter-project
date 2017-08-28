73e17f46d6d6956de129a435cd2538f393ebe888
Benjamin Crysler <ben.crysler@cmich.edu>
2017-08-28 17:11:02 -0400

    2017_8_28_17:10

---
+++ access_feed.py
@@ -0,0 +1,17 @@
+import tweepy
+
+consumer_key = 'AhqRDo5NYd2XRP3G3apZar7JC'
+consumer_secret = 'jXlQVdMOYucOBiO6iXsmgH4ApyBfsjCSw37fjmuiToAr6dCiFO'
+
+access_token = '68715828-WHXCGw6z3cLDj2EtudBqPggJWVSfCtHUkPra1SAjd'
+access_secret = 'vToagD83gYPHeGKqC9yBdUvFdcLh4KTl5ckqkmsMYX5FV'
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth)
+
+# public_tweets = api.search('Trump')
+#
+# for tweet in public_tweets:
+#     print(tweet.text)
