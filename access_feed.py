ddb9138162aedba4a8be16f6eb15725899041605
Benjamin Crysler <ben.crysler@cmich.edu>
2017-08-29 21:18:56 -0400

    2017_8_29_21:18

--- access_feed.py
+++ access_feed.py
@@ -1,5 +1,6 @@
 import tweepy
 
+# Lines 4-13 authenticate this script with author's registered twitter application and account."""
 consumer_key = 'AhqRDo5NYd2XRP3G3apZar7JC'
 consumer_secret = 'jXlQVdMOYucOBiO6iXsmgH4ApyBfsjCSw37fjmuiToAr6dCiFO'
 
@@ -11,7 +12,10 @@ auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
-public_tweets = api.search('Trump')
+# Examples of API calls
+trump_tweets = api.search('Trump')
+home_timeline = api.home_timeline(count=1)
 
-for tweet in public_tweets:
-    print(tweet.text)
+for tweet in home_timeline:
+    print(tweet)
+    # TODO: print this into a file, inserting newlines after each piece of data on the tweet
