28ee5e198a949d1b30863ce2c797d104b082908c
Benjamin Crysler <ben.crysler@cmich.edu>
2017-09-08 10:27:33 -0400

    2017_9_8_10:27
    Added more functionality for accessing tweet properties

--- access_feed.py
+++ access_feed.py
@@ -17,9 +17,10 @@ api = tweepy.API(auth)
 trump_tweets = api.search('Trump')
 home_timeline = api.home_timeline(count=25)
 
+# opening file for printing, utf-8 necessary for processing emoji
 output_file = io.open("results.txt", "w", encoding="utf-8")
 
-desired_properties = [0,1,2,3,4,5,6,7,8]
+desired_properties = [10,11,2,17]
 
 def getTweetInfo(tweet_set,indicies):
     # takes a tweet set and a set of indexes that reference tweet properties
@@ -33,9 +34,9 @@ def getTweetInfo(tweet_set,indicies):
                 output_file.write("Text: " + tweet.text + "\n")
             elif index == 3:
                 output_file.write("Truncated: " + str(tweet.truncated) + "\n")
-            # elif index == 4:
-            #     # TODO: break entities up
-            #     output_file.write("Entities: " + str(tweet.entities) + "\n")
+            elif index == 4:
+                # TODO: break entities up
+                output_file.write("Entities: " + str(tweet.entities) + "\n")
             elif index == 5:
                 output_file.write("Source App: " + tweet.source + "\n")
             elif index == 6:
@@ -47,11 +48,37 @@ def getTweetInfo(tweet_set,indicies):
                 else:
                     output_file.write("not a reply\n")
             elif index == 8:
-                reply_to_user_id = tweet.in_reply_to_user_id
+                reply_to_user_id = tweet.in_reply_to_user_id_str
                 if not reply_to_user_id == None:
                     output_file.write("In Reply to User ID: " + reply_to_user_id + "\n")
                 else:
                     output_file.write("not a reply\n")
+            elif index == 9:
+                output_file.write("User ID: " + tweet.user.id_str + "\n")
+            elif index == 10:
+                output_file.write("User Name: " + tweet.user.name + "\n")
+            elif index == 11:
+                output_file.write("User Handle: @" + tweet.user.screen_name + "\n")
+            elif index == 12:
+                output_file.write("User Location: " + tweet.user.location + "\n")
+            elif index == 13:
+                output_file.write("User Description: " + tweet.user.description + "\n")
+            elif index == 14:
+                user_site = tweet.user.url
+                if not tweet.user.url == None:
+                    output_file.write("User Website: " + tweet.user.url + "\n")
+                else:
+                    output_file.write("User does not supply site")
+            elif index == 15:
+                output_file.write("User Protected?: " + str(tweet.user.protected) + "\n")
+            elif index == 16:
+                output_file.write("User Followers: " + str(tweet.user.followers_count) +"\n")
+            elif index == 17:
+                output_file.write("User Friends: " + str(tweet.user.friends_count) + "\n")
+            else:
+                print ("invalid index")
+
+
 
         output_file.write("-----------------------------------------------------\n")
 
