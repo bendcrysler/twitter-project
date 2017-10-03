7a43f0b57ec9b5d3e324c2b39270ba2f2751a938
Benjamin Crysler <ben.crysler@cmich.edu>
2017-10-03 18:50:54 -0400

    2017_10_3_18:50

--- main.py
+++ main.py
@@ -12,27 +12,101 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 # establish SQLite connection
-conn = sqlite3.connect('twitter.db')
+conn = sqlite3.connect(':memory:')
 if (conn):
     print ("connection successful")
 else:
     print ("connection failed")
 curs = conn.cursor()
 
-# opening file for printing, utf-8 necessary for processing emoji
-output_file = io.open("results.txt", "w", encoding="utf-8")
+# create tables in DB if they don't already exist
+curs.execute("""CREATE TABLE
+                IF NOT EXISTS tweets (
+                    id text PRIMARY KEY NOT NULL,
+                    tweet_text text NOT NULL,
+                    user_id text NOT NULL REFERENCES users(id),
+                    time_posted text NOT NULL,
+                    hearts integer NOT NULL,
+                    retweets integer NOT NULL,
+                    retweeted_tweet text REFERENCES tweets(id),
+                    quoted_tweet text REFERENCES tweets(id),
+                    in_reply_to_tweet text REFERENCES tweets(id),
+                    in_reply_to_user text REFERENCES users(id),
+                    source_app text NOT NULL,
+                    source_app_url text NOT NULL,
+                    coordinates text,
+                    place text,
+                    language text NOT NULL,
+                    contributors text,
+                    truncated integer NOT NULL,
+                    dev_heart integer NOT NULL,
+                    dev_retweet integer NOT NULL
+                )""")
+curs.execute("""CREATE TABLE
+                IF NOT EXISTS users (
+                    id text PRIMARY KEY NOT NULL,
+                    name text NOT NULL,
+                    handle text NOT NULL,
+                    location text,
+                    description text NOT NULL,
+                    website text,
+                    protected integer NOT NULL,
+                    followers integer NOT NULL,
+                    following integer NOT NULL,
+                    listed integer NOT NULL,
+                    created_at text NOT NULL,
+                    favorites integer NOT NULL,
+                    utc_offset integer,
+                    timezone text,
+                    geo_enabled integer NOT NULL,
+                    verified integer NOT NULL,
+                    tweets integer NOT NULL,
+                    language text NOT NULL,
+                    contributors_enabled integer NOT NULL,
+                    translator integer NOT NULL,
+                    translation_enabled integer NOT NULL,
+                    extended_profile integer NOT NULL,
+                    default_profile integer NOT NULL,
+                    default_avatar integer NOT NULL,
+                    dev_follow integer NOT NULL,
+                    translator_type text
+                    )""")
+curs.execute("""CREATE TABLE
+                IF NOT EXISTS hashtags (
+                    tweet_id text NOT NULL REFERENCES tweets(id) ON UPDATE CASCADE,
+                    hashtag text NOT NULL,
+                    PRIMARY KEY(tweet_id, hashtag)
+                )""")
+curs.execute("""CREATE TABLE
+                IF NOT EXISTS mentions (
+                    tweet_id text NOT NULL REFERENCES tweets(id) ON UPDATE CASCADE,
+                    mention_handle text NOT NULL REFERENCES users(handle) ON UPDATE CASCADE,
+                    PRIMARY KEY (tweet_id, mention_handle)
+                )""")
 
 # Collect tweets
 trump_tweets = api.search('Trump')
 home_timeline = api.home_timeline(count=25)
 
-def getTweetInfo(tweet_set,indicies):
+def getTweetInfo(tweet_set):
     # inputs a tweet set into DB
     for tweet in tweet_set:
         # set variables to be inserted into DB
         ID = tweet.id_str
         text = tweet.text
-        user_id = tweet.user.id_str
+        u_id = tweet.user.id_str
+        u_name = tweet.user.name
+        u_handle = tweet.user.screen_name
+        u_description = tweet.user.description
+        u_protected = tweet.user.protected
+        u_followers = tweet.user.followers_count
+        u_following = tweet.user.friends_count
+        u_listed = tweet.user.listed_count
+        u_created_at = tweet.user.created_at
+        u_favorites = tweet.user.favourites_count
+        u_tweets = tweet.user.statuses_count
+        u_language = tweet.user.lang
+        u_contributors_enabled = tweet.user.contributors_enabled
         time_posted = str(tweet.created_at)
         hearts = tweet.favorite_count
         retweets = tweet.retweet_count
@@ -77,15 +151,79 @@ def getTweetInfo(tweet_set,indicies):
             dev_retweet = 1
         else:
             dev_retweet = 0
+        if hasattr(tweet, 'user.location'):
+            u_location = tweet.user.location
+        else:
+            u_location = None
+        if hasattr(tweet, 'user.url'):
+            u_website = tweet.user.url
+        else:
+            u_website = None
+        if hasattr(tweet, 'user.utc_offset'):
+            u_utc_offset = tweet.user.utc_offset
+        else:
+            u_utc_offset = None
+        if hasattr(tweet, 'user.timezone'):
+            u_timezone = tweet.user.timezone
+        else:
+            u_timezone = None
+        if tweet.user.geo_enabled == True:
+            u_geo_enabled = 1
+        else:
+            u_geo_enabled = 0
+        if tweet.user.verified == True:
+            u_verified = 1
+        else:
+            u_verified = 0
+        if tweet.user.contributors_enabled == True:
+            u_contributors_enabled = 1
+        else:
+            u_contributors_enabled = 0
+        if tweet.user.is_translator == True:
+            u_is_translator = 1
+        else:
+            u_is_translator = 2
+        if tweet.user.is_translation_enabled == True:
+            u_translation_enabled = 1
+        else:
+            u_translation_enabled = 0
+        if tweet.user.has_extended_profile == True:
+            u_extended_profile = 1
+        else:
+            u_extended_profile = 0
+        if tweet.user.default_profile == True:
+            u_default_profile = 1
+        else:
+            u_default_profile = 0
+        if tweet.user.default_profile_image == True:
+            u_default_avatar = 1
+        else:
+            u_default_avatar = 0
+        if tweet.user.following == True:
+            u_dev_follow = 1
+        else:
+            u_dev_follow = 0
+        if hasattr(tweet, 'user.translator_type'):
+            u_translator_type = tweet.user.translator_type
+        else:
+            u_translator_type = None
 
-        #set query using variables and run
-        query = "INSERT INTO tweets VALUES(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19)"
-        variables = {'1':ID, '2':text, '3':user_id, '4':time_posted, '5':hearts, '6':retweets, '7':retweeted_tweet, '8':quoted_tweet, '9': in_reply_to_tweet, '10':in_reply_to_user, '11':source_app, '12':source_app_url, '13':coordinates, '14':place, '15':language, '16':contributors, '17':truncated, '18':dev_heart, '19':dev_retweet}
-        curs.execute(query, variables)
+        #set queries and variables, then run
+        query1 = "INSERT INTO tweets VALUES(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19)"
 
-getTweetInfo(home_timeline, desired_properties)
-getTweetInfo(trump_tweets, desired_properties)
+        query2 = "INSERT INTO users VALUES(:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11, :12, :13, :14, :15, :16, :17, :18, :19, :20, :21, :22, :23, :24, :25, :26)"
 
+        variables1 = {'1':ID, '2':text, '3':u_id, '4':time_posted, '5':hearts, '6':retweets, '7':retweeted_tweet, '8':quoted_tweet, '9': in_reply_to_tweet, '10':in_reply_to_user, '11':source_app, '12':source_app_url, '13':coordinates, '14':place, '15':language, '16':contributors, '17':truncated, '18':dev_heart, '19':dev_retweet}
+
+        variables2 = {'1':ID, '2':u_name, '3':u_handle, '4':u_location, '5':u_description, '6':u_website, '7':u_protected, '8':u_followers, '9':u_following, '10':u_listed, '11':u_created_at, '12':u_favorites, '13':u_utc_offset, '14':u_timezone, '15':u_geo_enabled, '16':u_verified, '17':u_tweets, '18':u_language, '19':u_contributors_enabled, '20':u_is_translator, '21':u_translation_enabled, '22':u_extended_profile, '23':u_default_profile, '24':u_default_avatar, '25':u_dev_follow, '26':u_translator_type}
+
+        curs.execute(query1, variables1)
+        curs.execute(query2, variables2)
+
+# insert tweet and user sets into DB
+getTweetInfo(home_timeline)
+getTweetInfo(trump_tweets)
+
+# commit to DB and close connection
 conn.commit()
 conn.close()
-output_file.close()
